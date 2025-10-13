20eb45f69ac7255e600f8c00e440b1c1706e893a
DivyanshuTyagi <tyagidivyanshu3090@gmail.com>
2025-10-13 15:58:03 +0530

    Connecting to the DB

--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -6,31 +6,31 @@ const app = express(); //  Creating the instance of express server -> app server
 
 const PORT = 3000;
 
-// Middleware
-app.use("/devtinder", checkAuth);
-
-// Separate route
-app.get("/user/getData", userAuth, (req, res) => {
-  res.send("The user name is Divyanshu tyagi");
-});
-
-app.get("/devtinder/getuser", (req, res) => {
-  res.send({
-    name: "Divyanshu Tyagi",
-    occupation: "Software Developer",
-  });
-});
-
-app.delete("/devtinder/deleteuser", (req, res) => {
-  res.send("The user is deleted");
-});
-
 connectToDB()
-  .then(
+  .then(() => {
     app.listen(PORT, (req, res) => {
       console.log(`The server is running at port number ${PORT}`);
-    })
-  )
+    });
+  })
   .catch((err) => {
     console.log("error received");
   });
+
+// // Middleware
+// app.use("/devtinder", checkAuth);
+
+// // Separate route
+// app.get("/user/getData", userAuth, (req, res) => {
+//   res.send("The user name is Divyanshu tyagi");
+// });
+
+// app.get("/devtinder/getuser", (req, res) => {
+//   res.send({
+//     name: "Divyanshu Tyagi",
+//     occupation: "Software Developer",
+//   });
+// });
+
+// app.delete("/devtinder/deleteuser", (req, res) => {
+//   res.send("The user is deleted");
+// });
--- DevTinder/src/config/database.js
+++ DevTinder/src/config/database.js
@@ -2,11 +2,9 @@ const mongoose = require("mongoose");
 
 async function connectToDB() {
   const response = await mongoose.connect(
-    "mongodb+srv://divyanshu422_db_user:Divyanshu3090@dev0tinder.wo2gq5y.mongodb.net/devTinder"
+    "mongodb+srv://divyanshu422_db_user:Divyanshu3090@devtinder.qapahyz.mongodb.net/"
   );
   console.log("database connected");
 }
-
-// connectToDB();
 
 module.exports = connectToDB; // "Here is a function for other files to use."
