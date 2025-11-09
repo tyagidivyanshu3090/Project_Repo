5483e9761f8c650676388b80c61ebf6a49f60bf8
DivyanshuTyagi <tyagidivyanshu3090@gmail.com>
2025-11-09 15:15:40 +0530

    Saving Hard coded data to database

--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -2,9 +2,24 @@ const express = require("express");
 const connectToDB = require("./config/database");
 const { checkAuth, userAuth } = require("./middleware/auth");
 
+const { UserModel } = require("./models/user");
+
 const app = express(); //  Creating the instance of express server -> app server
 
 const PORT = 3000;
+
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "Divyanshu",
+    lastName: "Tyagi",
+    emailId: "divyanshu422@gmail.com",
+    password: "abcd@1234",
+  };
+  // Creating the instance of userModal
+  const user = new UserModel(userObj);
+  await user.save();
+  res.send("Data saved to database");
+});
 
 connectToDB()
   .then(() => {
--- DevTinder/src/models/user.js
+++ DevTinder/src/models/user.js
@@ -23,4 +23,4 @@ const userSchema = new mongoose.Schema({
 
 const UserModel = mongoose.model("User", userSchema);
 
-module.exports = UserModel;
+module.exports = { UserModel };
