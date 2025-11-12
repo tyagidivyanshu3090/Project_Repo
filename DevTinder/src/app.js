2024de0917a2dde8fd442460de57f7f357fdfc67
DivyanshuTyagi <tyagidivyanshu3090@gmail.com>
2025-11-12 16:02:50 +0530

    Delete api

--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -50,6 +50,7 @@ app.get("/user", async (req, res) => {
   }
 });
 
+// Finding the user using the ID
 app.get("/userbyid", async (req, res) => {
   const UserById = req.body.userById;
   try {
@@ -57,6 +58,19 @@ app.get("/userbyid", async (req, res) => {
     res.status(200).send(user);
   } catch (err) {
     res.status(500).send("Something went wrong");
+  }
+});
+
+// Deleting the user using the id -> findByIdAndDelete
+app.delete("/deleteuser", async (req, res) => {
+  const userId = req.body.userById;
+  console.log(userId);
+  try {
+    const user = await UserModel.findByIdAndDelete(userId);
+    // console.log(user);
+    res.status(200).send("User deleted successfully");
+  } catch (err) {
+    res.status(500).send("Server error");
   }
 });
 
@@ -70,22 +84,3 @@ connectToDB()
   .catch((err) => {
     console.log("error received");
   });
-
-// // Middleware
-// app.use("/devtinder", checkAuth);
-
-// // Separate route
-// app.get("/user/getData", userAuth, (req, res) => {
-//   res.send("The user name is Divyanshu tyagi");
-// });
-
-// app.get("/devtinder/getuser", (req, res) => {
-//   res.send({
-//     name: "Divyanshu Tyagi",
-//     occupation: "Software Developer",
-//   });
-// });
-
-// app.delete("/devtinder/deleteuser", (req, res) => {
-//   res.send("The user is deleted");
-// });
