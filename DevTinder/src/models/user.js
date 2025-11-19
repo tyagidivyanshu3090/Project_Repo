c9cecb1b7cbe172808408d58315f1c8bf8ec31ce
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-11-19 07:06:01 +0530

    Validator library and using to check email and PhotoUrl

--- DevTinder/src/models/user.js
+++ DevTinder/src/models/user.js
@@ -1,57 +1,70 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const userSchema = new mongoose.Schema(
+  {
+    lastName: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      validate: {
+        validator: function (value) {
+          if (!validator.isEmail(value)) {
+            throw new Error("Invalid email address: " + value);
+          }
+          return true;
+        },
+      },
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        let allowedValue = ["male", "female", "other"];
+        if (!allowedValue.includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
 
-const userSchema = new mongoose.Schema({
-  
-  lastName: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true,
-    trim: true,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-    validate(value) {
-      let allowedValue = ["male", "female", "other"];
-      if (!allowedValue.includes(value)) {
-        throw new Error("Gender data is not valid");
-      }
+    // gender: {
+    //   type: String,
+    //   enum: ["male", "female", "other"]
+    // },
+
+    photoUrl: {
+      type: String,
+      default:
+        "https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "User description not provided",
+    },
+    skills: {
+      type: [String],
     },
   },
-
-  // gender: {
-  //   type: String,
-  //   enum: ["male", "female", "other"]
-  // },
-
-  photoUrl: {
-    type: String,
-    default:
-      "https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png",
-  },
-  about: {
-    type: String,
-    default: "User description not provided",
-  },
-  skills: {
-    type: [String],
-  },
-},{
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 const UserModel = mongoose.model("User", userSchema);
 
 module.exports = { UserModel };
-
-
