847eef1118a15de49f15e14d542ccee0da7f2adf
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-12-07 17:16:02 +0530

    feat: Add API endpoint for changing user passwords with old password validation and new password hashing.

--- DevTinder/src/routes/profileRouter.js
+++ DevTinder/src/routes/profileRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
 const validateEditProfileData = require("../utilsOrHelperFolder/validateEditProfileData");
+const bcrypt = require("bcrypt");
 
 // view profile -> meaning sending the user details to frontend
 profileRouter.get("/view", userAuth, async (req, res) => {
@@ -40,6 +41,43 @@ profileRouter.patch("/edit", userAuth, async (req, res) => {
     res
       .status(401)
       .send({ message: "Error in editing the data", error: err.message });
+  }
+});
+
+// changePassword -> meaning allowing the user for updating the password
+// Change Password API (often confused with "Forgot Password," but since the user is logged in, it's technically "Change Password").
+// User will send the oldPassword and newPassword
+profileRouter.patch("/changepassword", userAuth, async (req, res) => {
+  try {
+    // 1. Extract the oldPassword and newPassword from the request body
+    const { oldPassword, newPassword } = req.body;
+    // 2. Validate the Request Body
+    if (!oldPassword || !newPassword) {
+      throw new Error("Please provide the password");
+    }
+    if (oldPassword === newPassword) {
+      throw new Error("New password should be different from old password");
+    }
+
+    const user = req.user;
+    const isPasswordMatched = await bcrypt.compare(oldPassword, user.password);
+    if (!isPasswordMatched) {
+      throw new Error("Invalid password");
+    }
+
+    // 3. Hash the new password
+    user.password = await bcrypt.hash(newPassword, 10);
+    // 4. Saving the new password in the database
+    await user.save();
+    // 5. Send Success Response
+    res.json({
+      message: "Password changed successfully",
+      data: user,
+    });
+  } catch (err) {
+    res
+      .status(401)
+      .send({ message: "Error in changing the password", error: err.message });
   }
 });
 
