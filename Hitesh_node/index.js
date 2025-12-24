08f7ff3a51610111f527f55c4a0843b1d7946842
DivyanshuTyagi <tyagidivyanshu3090@gmail.com>
2025-12-24 17:13:00 +0530

    feat: Add file writing functionality to `index.js` and create `createdFile.txt`.

--- Hitesh_node/index.js
+++ Hitesh_node/index.js
@@ -1,5 +1,9 @@
 const fs = require("node:fs");
 
 // Reading the file using sync method which takes 2 parameters: file name and character encoding format
-const content = fs.readFileSync("note.txt", "utf-8");
-console.log(content);
+// const content = fs.readFileSync("note.txt", "utf-8");
+// console.log(content);
+
+// Creating the file using sync method
+
+fs.writeFileSync("createdFile.txt", "Hello World", "utf-8");
