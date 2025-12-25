3a878abd7b31b8b6f1cfebe503ce88d32abfeea9
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-12-25 18:28:05 +0530

    Adding comment

--- Hitesh_node/index.js
+++ Hitesh_node/index.js
@@ -1,9 +1,13 @@
 const fs = require("node:fs");
 
 // Reading the file using sync method which takes 2 parameters: file name and character encoding format
+
 // const content = fs.readFileSync("note.txt", "utf-8");
 // console.log(content);
 
-// Creating the file using sync method
+// ------------------------------------------------------
+
+// Creating the file using sync method: takes 3 parameters: file name, content, and character encoding format.
+// for creating a new file, we use writeFileSync method not createFileSync as it is not a built-in method. 
 
 fs.writeFileSync("createdFile.txt", "Hello World", "utf-8");
